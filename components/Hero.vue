f1a7bc977c38792c1b129313d36bcfa1f3f4d764
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-07-01 20:00:34 +0300

    Hero grid

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,41 +1,49 @@
 <template>
     <div class="hero container">
         <div class="hero__content-wrapper">
-            <h1 class="title-h3 split-type animate__title-in">Naturally comforting.<br>Timelessly modern.</h1>
+            <h1 class="title-h2 split-type animate__title-in">Naturally comforting.<br>Timelessly modern.</h1>
             <p class="hero__description split-type animate__subtitle-in mt-8">We pride ourselves on creating furniture that is not only visually stunning but also built to last. Each piece meticulously crafted by artisans who bring generations of expertise to their work.</p>
         </div>
         <div class="hero__bottom-wrapper pb-4 pb-md-16">
-            <ButtonMain  class="title-h5" text="Scroll down" :has-icon="true" icon="IconArrowNarrowDown"/>
+            <ButtonMain  class="title-h5 animate__scroll-down-in" text="Scroll down" :has-icon="true" icon="IconArrowNarrowDown"/>
         </div>
-        <div class="hero__img-container container pt-4 pt-md-6 pb-4 pb-md-16">
-            <nuxt-img
-                src="/migrant-9.jpg"
-                class="hero__img--1 img" 
-                sizes="sm:30vw md:30vw lg:30vw 30vwv"
-                role="presentation"
-                loading="lazy"
-            />
-            <nuxt-img
-                src="/migrant-2.jpg"
-                class="hero__img--2 img" 
-                sizes="sm:30vw md:30vw lg:30vw 30vwv"
-                role="presentation"
-                loading="lazy"
-            />
-            <nuxt-img
-                src="/migrant-4.jpg"
-                class="hero__img--3 img" 
-                sizes="sm:30vw md:30vw lg:30vw 30vwv"
-                role="presentation"
-                loading="lazy"
-            />
-            <nuxt-img
-                src="/migrant-6.jpg"
-                class="hero__img--4 img" 
-                sizes="sm:30vw md:30vw lg:30vw 30vwv"
-                role="presentation"
-                loading="lazy"
-            />
+        <div class="hero__img-container container animate__cards-in pt-4 pt-md-6 pb-4 pb-md-16">
+            <div class="hero__card animate__card hero__card--1">
+                <nuxt-img
+                    src="/migrant-9.jpg"
+                    class="img" 
+                    sizes="sm:30vw md:30vw lg:20vw 30vwv"
+                    role="presentation"
+                    loading="lazy"
+                />
+            </div>
+            <div class="hero__card animate__card hero__card--2">
+                <nuxt-img
+                    src="/migrant-2.jpg"
+                    class="img" 
+                    sizes="sm:30vw md:30vw lg:20vw 30vwv"
+                    role="presentation"
+                    loading="lazy"
+                />
+            </div>
+            <div class="hero__card animate__card hero__card--3">
+                <nuxt-img
+                    src="/migrant-4.jpg"
+                    class="img" 
+                    sizes="sm:30vw md:30vw lg:20vw 30vwv"
+                    role="presentation"
+                    loading="lazy"
+                />
+            </div>
+            <div class="hero__card animate__card hero__card--4">
+                <nuxt-img
+                    src="/migrant-6.jpg"
+                    class="img" 
+                    sizes="sm:30vw md:30vw lg:20vw 30vwv"
+                    role="presentation"
+                    loading="lazy"
+                />
+            </div>
         </div>
 
     </div>
@@ -46,7 +54,26 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+:root {
+    --img-card-1-x: 10;
+    --img-card-1-y: 10;
+    --img-card-1-width: 20vw;
+    --img-card-1-aspect-ratio: 16/9;
+    --img-card-2-x: 82;
+    --img-card-2-y: 18;
+    --img-card-2-width: 15vw;
+    --img-card-2-aspect-ratio: 9/9;
+    --img-card-3-x: 4;
+    --img-card-3-y: 64;
+    --img-card-3-width: 15vw;
+    --img-card-3-aspect-ratio: 9/9;
+    --img-card-4-x: 64;
+    --img-card-4-y: 72;
+    --img-card-4-width: 20vw;
+    --img-card-4-aspect-ratio: 9/12;
+}
+
 .hero {
     height: 100lvh;
 
@@ -74,27 +101,34 @@
         position: absolute;
         inset: 0;
         z-index: -1;
-        display: grid;
-        grid-template-rows: repeat(var(--grid-columns), minmax(0, 1fr));
-        grid-template-columns: repeat(var(--grid-columns), minmax(0, 1fr));
-        grid-gap: var(--column-gap);
     }
-    &__img {
+    &__card {
+        position: absolute;
+        transform: translate(0%, 0%);
+
         &--1 {
-            grid-column: 2 / span 3;
-            grid-row: 2 / span 3;
+            left: calc(1% * var(--img-card-1-x));
+            top: calc(1% * var(--img-card-1-y));
+            width: var(--img-card-1-width);
+            aspect-ratio: var(--img-card-1-aspect-ratio);
         }
         &--2 {
-            grid-column: 11 / span 2;
-            grid-row: 3 / span 3;
+            left: calc(1% * var(--img-card-2-x));
+            top: calc(1% * var(--img-card-2-y));
+            width: var(--img-card-2-width);
+            aspect-ratio: var(--img-card-2-aspect-ratio);
         }
         &--3 {
-            grid-column: 1 / span 2;
-            grid-row: 9 / span 2;
+            left: calc(1% * var(--img-card-3-x));
+            top: calc(1% * var(--img-card-3-y));
+            width: var(--img-card-3-width);
+            aspect-ratio: var(--img-card-3-aspect-ratio);
         }
         &--4 {
-            grid-column: 9/ span 3;
-            grid-row: 10 / span 3;
+            left: calc(1% * var(--img-card-4-x));
+            top: calc(1% * var(--img-card-4-y));
+            width: var(--img-card-4-width);
+            aspect-ratio: var(--img-card-4-aspect-ratio);
         }
     }
 }
--- composables/use-page-loader-animate.js
+++ composables/use-page-loader-animate.js
@@ -28,11 +28,67 @@ export default () => {
     function timelineLoaderOutAnimations () {
         const tl = gsap.timeline()  
 
+        if(document.querySelector('.animate__cards-in'))
+            tl.add(timelineCardsIn(), '+=2')
+
         if(document.querySelector('.animate__title-in'))
-            tl.add(timelineTitleIn(), '-=1.6s')
+            tl.add(timelineTitleIn(), '-=1.1')
+
+        if(document.querySelector('.animate__subtitle-in'))
+            tl.add(timelineSubtitleIn(), '-=1.6')
 
         if(document.querySelector('.animate__header-in'))
             tl.add(timelineHeaderIn(), '<')
+
+        if(document.querySelector('.animate__scroll-down-in'))
+            tl.add(timelineScrollDownIn(), '<')
+
+        return tl
+    }
+
+    // Animate cards
+    function timelineCardsIn (){
+        const tl = gsap.timeline({
+            defaults: {
+                duration: 3,
+                ease: 'expo.inOut'
+            },
+        })  
+
+        tl.fromTo('.animate__cards-in .animate__card img', { 
+            clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)',
+            scale: 1.2
+        }, {
+            scale: 1,
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+            duration: 1.3,
+            stagger: 0.8,
+            ease: "expo.out",
+        })
+
+
+        tl.from('.animate__cards-in .animate__card', { 
+            '--img-card-1-x': '50',
+            '--img-card-1-y': '50',
+            '--img-card-1-width': '18vw',
+            '--img-card-1-aspect-ratio': '9/12',
+            '--img-card-2-y': '50',    
+            '--img-card-2-x': '50',
+            '--img-card-2-width': '19vw',
+            '--img-card-2-aspect-ratio': '9/12',
+            '--img-card-3-y': '50',  
+            '--img-card-3-x': '50',
+            '--img-card-3-width': '20vw',
+            '--img-card-3-aspect-ratio': '9/12',
+            '--img-card-4-y': '50',  
+            '--img-card-4-x': '50', 
+            '--img-card-4-width': '21vw',
+            '--img-card-4-aspect-ratio': '9/12',
+            
+            yPercent: -50,
+            xPercent: -50,  
+        })
+        
 
         return tl
     }
@@ -68,6 +124,7 @@ export default () => {
         tl
             .from('.animate__title-in .split-type--word', { 
                 yPercent: 100,
+                rotate: 0.1,
                 stagger: 0.04,
             })
 
@@ -84,6 +141,22 @@ export default () => {
 
         tl
             .from('.animate__subtitle-in ', { 
+                opacity: 0
+            })
+
+        return tl
+    }
+
+    function timelineScrollDownIn (){
+        const tl = gsap.timeline({
+            defaults: {
+                duration: 1.2,
+                ease: "power2.out"
+            },
+        })  
+
+        tl
+            .from('.animate__scroll-down-in', { 
                 opacity: 0
             })
 
